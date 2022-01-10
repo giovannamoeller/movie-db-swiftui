2531fb1b025d216ee1ccbc55180401e99466f637
giovannamoeller <giovannagmoeller@gmail.com>
2022-01-10 20:08:07 -0300

    Initial Commit

---
+++ MovieAppSwiftUI/MovieAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppSwiftUIApp.swift
+//  MovieAppSwiftUI
+//
+//  Created by Giovanna Moeller on 10/01/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
