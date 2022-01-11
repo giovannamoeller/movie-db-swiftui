6f6b013a198bfc49e102c6a2e2bb382b38e1b8f5
giovannamoeller <giovannagmoeller@gmail.com>
2022-01-10 22:29:08 -0300

    setting api key into .plist file

--- MovieAppSwiftUI/Services/API.swift
+++ MovieAppSwiftUI/Services/API.swift
@@ -9,7 +9,13 @@ import Foundation
 import Alamofire
 
 struct API {
+  
   static func makeRequest() {
+    
+    guard let path = Bundle.main.path(forResource: "keys", ofType: "plist") else { fatalError("API Key Path Not Found!")}
+    let keys = NSDictionary(contentsOfFile: path)
+    let APIKey = keys!["API_KEY"]!
+    
     AF.request("url").response { response in
       print(response)
     }
