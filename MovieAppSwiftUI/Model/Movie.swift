900351f4d38b450056a25cfab848c51f89d42483
giovannamoeller <giovannagmoeller@gmail.com>
2022-01-10 21:00:54 -0300

    creating movie model

---
+++ MovieAppSwiftUI/Model/Movie.swift
@@ -0,0 +1,18 @@
+//
+//  Movie.swift
+//  MovieAppSwiftUI
+//
+//  Created by Giovanna Moeller on 10/01/22.
+//
+
+import Foundation
+
+// spider man ID movie from movieDB: 634649
+
+struct Movie {
+  var name: String
+  var image: String
+  var numberOfLikes: Int
+  var popularity: Int
+  //var similarMovies: [SimilarMovie]
+}
